b0300a9bec25509f3194363ee6ee13475071d8cf
Emtiaz Siddique <siddiqueemtiaz@gmail.com>
2026-04-29 00:48:54 +0600

    add/update for object

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+// const post = {
+//     username: "@Emtiazz",
+//     content: "This is my first post",
+//     likes: 2400,
+//     repost: 45,
+//     comments: 20,
+//     tags: ["new", "post", "good"]
+// }
+
+// console.log(post);
+
+
+//how to get values from object
+// post["content"]
+// post.content
+
+// const obj = {
+//     1: "a",
+//     2: "b",
+//     true: "c",
+//     null: "d",
+//     undefined: "e"
+
+// }
 const student = {
     name: "Emtiazz",
-    age: 21,
-    id: 301,
-    cgpa: 3.7,
-    city: "Dhaka"
+    age: 22,
+    cgpa: 3.75,
+    city: "Dhaka",
 };
 
-
-
-
+//changing city
+student.city = "Chittagong";
+student.gender = "Male";
